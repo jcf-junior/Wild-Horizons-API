dea75090c04b0250630ea82d46b81266103749ff
jcf-junior <h1ddenntc@gmail.com>
2025-06-23 17:56:51 +0100

    Enabled CORS

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,9 @@
 export function sendJSONResponse(response, statusCode = 200, data, contentType = "application/json") {
 
+    //CORS
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    response.setHeader("Access-Control-Allow-Methods", "GET")
+    //
     response.setHeader("Content-Type", contentType)
     response.statusCode = statusCode
     response.end(JSON.stringify(data))
