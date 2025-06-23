0b88582bd21d7c7eba0b4e28d1ef01ca83495b1b
jcf-junior <h1ddenntc@gmail.com>
2025-06-23 17:13:35 +0100

    Added db.js

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-import http from "node:http"
-
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+import { sendResponse } from "./utils/sendResponse.js"
 const PORT = 8000
 
-const server = http.createServer( (req, res) => {
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
 
-    res.end("Hello from the server")
- 
-} )
+  if (req.url === '/api' && req.method === 'GET') {
 
-server.listen(PORT, () => console.log(`Server connected on port: ${PORT}`))
+    sendResponse(res, destinations)
+
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+    const continent = req.url.split('/').pop()
+    const filteredData = destinations.filter((destination) => {
+      return destination.continent.toLowerCase() === continent.toLowerCase()
+    })
+    
+    sendResponse(res, filteredData)
+
+  } else {
+
+    const data = {
+      error: "not found",
+      message: "The requested route does not exist"
+    }
+
+    sendResponse(res, data, 404)
+  }
+  
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
